d548d2e0ff1795cf48eac027fddcc7a737a357c5
Liane Hampe <liaham@xmera.de>
2023-01-16 15:28:17 +0100

    Updates Copyright year
    Implements #1442

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@
 
 # This file is part of the Plugin Redmine Reports.
 #
-# Copyright (C) 2021 Liane Hampe <liaham@xmera.de>, xmera.
+# Copyright (C) 2021-2023 Liane Hampe <liaham@xmera.de>, xmera Solutions GmbH.
 #
 # This plugin program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
