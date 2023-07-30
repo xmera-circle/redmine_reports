e521ed6798d2e3e91614b6a12130a4e02c1975a1
Liane Hampe <liaham@xmera.de>
2023-07-30 18:51:52 +0200

    Bumps minor version to 0.2.0

--- init.rb
+++ init.rb
@@ -22,7 +22,7 @@ Redmine::Plugin.register :redmine_reports do
   name 'Redmine Reports'
   author 'Liane Hampe, xmera Solutions GmbH'
   description 'Simple bar chart reports of issues'
-  version '0.1.2'
+  version '0.2.0'
   url 'https://circle.xmera.de/projects/redmine-reports'
   author_url 'http://xmera.de'
 
